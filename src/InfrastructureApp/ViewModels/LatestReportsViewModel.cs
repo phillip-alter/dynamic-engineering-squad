01a0acdbfc2b21f026df10d10d976473d224763b
Julian Aguilar-Tomas <julianaguilar.tomas17@gmail.com>
2026-02-09 00:23:07 -0800

    Added ViewModels to represent reports list and individual report items for the Latest Reports view.

--- src/InfrastructureApp/ViewModels/LatestReportsViewModel.cs
+++ src/InfrastructureApp/ViewModels/LatestReportsViewModel.cs
@@ -5,5 +5,11 @@ namespace InfrastructureApp.ViewModels
         public List<LatestReportItemViewModel> Reports { get; set; } = new();
     }
 
-
+    public class LatestReportItemViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = "";
+        public string Status { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
 }
