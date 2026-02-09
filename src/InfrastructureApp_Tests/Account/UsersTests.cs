1abafa75e35e468bdab585ee41383bd154bb68ad
Phillip Alter <phillip.alter@outlook.com>
2026-02-09 15:47:30 -0800

    (add) add username normalization test

--- src/InfrastructureApp_Tests/Account/UsersTests.cs
+++ src/InfrastructureApp_Tests/Account/UsersTests.cs
@@ -22,4 +22,20 @@ public class UsersTests
         Assert.That(usersModel.NormalizedUserName, Is.EqualTo(userName.ToUpper()));
         Assert.That(usersModel.UserName, Is.EqualTo(userName));
     }
+    
+    [Test]
+    public void Users_ChecksEmail_EmailIsNormalized()
+    {
+        string userName = "testUser";
+        string email = "testUser@test.com";
+        
+        usersModel = new Users(userName, email)
+        {
+            UserName = userName,
+            Email = email
+        };    
+        
+        Assert.That(usersModel.NormalizedEmail, Is.EqualTo(email.ToUpper()));
+        Assert.That(usersModel.Email, Is.EqualTo(email));
+    }
 }
