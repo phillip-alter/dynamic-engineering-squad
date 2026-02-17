14ec08e19f53c0cb972d88c6c3709f07a8034eef
Phillip Alter PC <phillip.alter@outlook.com>
2026-02-17 09:23:26 -0800

    (add) add properties and annotations to viewmodel

--- src/InfrastructureApp/ViewModels/Account/LoginViewModel.cs
+++ src/InfrastructureApp/ViewModels/Account/LoginViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace InfrastructureApp.ViewModels.Account;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InfrastructureApp.ViewModels.Account;
 
 public class LoginViewModel
 {
+    [Required(ErrorMessage = "Username is required")]
+    public string Username { get; set; }
     
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
 }
