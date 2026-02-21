8880a86072259fc2a903efba49a485ccbc48272f
Sunair Iqbal <siqbal24@mail.wou.edu>
2026-02-20 23:25:39 -0800

    adding comments to clarify code

--- src/InfrastructureApp/Configuration/GoogleMapsOptions.cs
+++ src/InfrastructureApp/Configuration/GoogleMapsOptions.cs
@@ -1,3 +1,6 @@
+//This file defines a configuration class (also called an Options class).
+//Its purpose is to store settings for Google Maps in a strongly-typed way.
+
 namespace InfrastructureApp.Configuration
 {
     public class GoogleMapsOptions
