5324381ff6f6223cb8b9216303e0b7043f210076
Phillip Alter PC <phillip.alter@outlook.com>
2026-03-04 15:47:51 -0800

    (add) add IUserService with GetUsersWithRolesAsync

---
+++ src/InfrastructureApp/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using InfrastructureApp.Models;
+using InfrastructureApp.ViewModels.Account;
+
+namespace InfrastructureApp.Services;
+
+public interface IUserService
+{
+    public Task<PaginatedList<AdminViewModel>>
+        GetUsersWithRolesAsync(int page, int pageSize);
+}
