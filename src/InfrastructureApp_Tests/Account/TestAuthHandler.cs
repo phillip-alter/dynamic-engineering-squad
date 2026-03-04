5ec65231755137f8ac2fe5a39b494fb790019a9e
Phillip Alter PC <phillip.alter@outlook.com>
2026-03-04 13:25:26 -0800

    (add) test auth handler for simulating auth'd user

---
+++ src/InfrastructureApp_Tests/Account/TestAuthHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace InfrastructureApp_Tests;
+
+public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options,logger,encoder){ }
+    
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        // fake user with name claim.
+        var claims = new[] { new Claim(ClaimTypes.Name, "TestUser") };
+        var identity = new ClaimsIdentity(claims, "Test");
+        var principal = new ClaimsPrincipal(identity);
+            
+        // generating success ticket.
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
