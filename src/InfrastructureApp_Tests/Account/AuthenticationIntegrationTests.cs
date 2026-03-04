d2544aa147caa4fa8f49dc3dab73b08223645c4f
Phillip Alter PC <phillip.alter@outlook.com>
2026-03-04 12:52:12 -0800

    (add) add set up and tear down methods

---
+++ src/InfrastructureApp_Tests/Account/AuthenticationIntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace InfrastructureApp_Tests;
+
+[TestFixture]
+public class AuthenticationIntegrationTests
+{
+    private WebApplicationFactory<Program> _factory;
+    private HttpClient _client;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new WebApplicationFactory<Program>();
+        _client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false
+        });
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _client.Dispose();
+        _factory.Dispose();
+    }
+}
