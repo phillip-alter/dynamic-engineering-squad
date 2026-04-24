1ad5d6f776fe884e6a5e22674b2e55165bb6afe2
Julian Aguilar-Tomas <julianaguilar.tomas17@gmail.com>
2026-04-24 15:44:48 -0700

    Added first Home relative time test

--- src/InfrastructureApp_Tests/HomeReportRelativeTimeTests.cs
+++ src/InfrastructureApp_Tests/HomeReportRelativeTimeTests.cs
@@ -1,3 +1,4 @@
+using InfrastructureApp.Models;
 using NUnit.Framework;
 
 namespace InfrastructureApp_Tests
@@ -5,6 +6,17 @@ namespace InfrastructureApp_Tests
     [TestFixture]
     public class HomeReportRelativeTimeTests
     {
-        
+        // -------------------------------------------------------
+        // TEST 1: Reports created less than one minute ago show "Just now"
+        // -------------------------------------------------------
+        [Test]
+        public void BuildRelativeTime_LessThanOneMinuteAgo_ReturnsJustNow()
+        {
+            var createdAt = DateTime.UtcNow.AddSeconds(-30);
+
+            var result = ReportIssue.BuildRelativeTime(createdAt, DateTime.UtcNow);
+
+            Assert.That(result, Is.EqualTo("Just now"));
+        }
     }
 }
